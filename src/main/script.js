f81bcbe0ed9d29a71601eaa4b2f015f314ff9691
Wesley Selmer <wesleyselmer@gmail.com>
2024-03-19 16:56:48 -0300

    Dropdown e Responsividade

--- src/main/script.js
+++ src/main/script.js
@@ -9,8 +9,11 @@ function carrossel () {
     if(fotoAtual > img.length - 1) {
         fotoAtual = 0;
     }
-
-    imgs.style.transform = `translateX(${-fotoAtual * 267}px)`;
+    if(window.innerWidth > 500) {
+        imgs.style.transform = `translateX(${-fotoAtual * 267}px)`;
+    } else {
+        imgs.style.transform = `translateX(${-fotoAtual * 230}px)`;
+    }
 }
 
 setInterval(carrossel, 1800);
